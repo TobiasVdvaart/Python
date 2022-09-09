f95cf2d89dc701cb36be19d4c3617665ec678dc0
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-09-09 11:17:07 +0200

    spieken

---
+++ spiekbriefje.py
@@ -0,0 +1,8 @@
+# spiekbriefje
+# ######################################################################### # variabelen ######################################################################### #
+#  voorbeeld aantal = 6 # dit is een integer
+#  ######################################################################### # 
+# integer is een heel getal zonder komma. # string is een zin of een word # float is een getal met komma # als je print gebruik je alleen bij een string komma's aan het begin en eind derest gebruik je gwn alleen tussen haakjes
+#   ######################################################################### # built in functies ######################################################################### # 
+# type casting #  als je iets bijvoorbeeld een integer een string wilt maken doe je print (str (aantal) aantal staat voor dat je het wilt veranderen. + ' ' + .....  # >>>
+#  print(str(aantal) + ' ' + omschrijving + ' ' + str(stuksprijs)) 3 # Croky naturel 300gr 0.89
