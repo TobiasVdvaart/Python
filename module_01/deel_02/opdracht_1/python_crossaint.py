b13cbe46dde687da4b847fd7f317abed4c73c73f
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-09-12 11:51:11 +0200

    opdracht 2

---
+++ module_01/deel_02/opdracht_1/python_crossaint.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+prijs_croissant = 0.39
+croissant = 17
+stokbroden = 2
+stokbroden_prijs = 2.78
+kortsingbonnen = 3*0.50
+
+
+print(f"De feestlunch kost je bij de bakker euro: {(croissant*prijs_croissant)+(stokbroden*stokbroden_prijs)-kortsingbonnen}")
