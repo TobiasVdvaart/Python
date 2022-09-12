0a7814986a444edc48c13b828a4ccd63c37d5c7c
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-09-12 12:15:37 +0200

    opdracht2

---
+++ module_01/deel_02/opdracht_1/NAW.py
@@ -0,0 +1,8 @@
+adres = """----------------------------------------------------
+|  Naam      : Nadir Ait
+|  Adres     : Koekenbakkersplein 23
+|  Postcode  : 2834 EF
+|  Woonplaats: Dordrecht
+ ----------------------------------------------------"""
+
+print(adres)
