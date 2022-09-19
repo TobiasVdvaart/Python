bacc12f9b742d5be2a929d1205878ce5e599cae2
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-09-19 11:21:00 +0200

    'opdracht2'

---
+++ module_01/deel_02/opdracht_1/Kaas, kaas en nog eens kaas!.py
@@ -0,0 +1,44 @@
+geel = input("Is de kaas geel? ")
+
+if geel == "nee":
+    schimmel = input("heeft de kaas blauwe schimmel?")
+
+    if schimmel == "ja":
+        korst = input("heeft de kaas een korst? ")
+
+        if korst == "ja":
+            print("Blue de Rochbaron")
+
+        else:
+            print("Foume d'Ambert")
+
+    else:
+        korst1 = input("heeft de kaas een korst? ")
+
+        if korst1 == "ja":
+            print("Camembert")
+
+        else:
+            print("Mozzarella")
+
+if geel == "ja":
+    gaten = input("Zitten er gaten in? ")
+
+    if gaten == "ja":
+        duur = input("Is de kaas belachelijk duur? ")
+
+        if duur == "ja":
+            print("Emmenthaler")
+
+        else:
+            print("Leerdammer")
+
+    else:
+        steen = input("Is de kaas hard als steen? ")
+
+        if steen == "ja":
+            print("Parmigiano Reggiano")
+
+        else:
+            print("Goudse kaas")
+
