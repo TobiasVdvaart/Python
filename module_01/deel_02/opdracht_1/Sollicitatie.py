5a367d0045a5ba6ed6941e03bc65ac96e527a56c
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-09-26 10:28:55 +0200

    'opdracht2'

--- module_01/deel_02/opdracht_1/Sollicitatie.py
+++ module_01/deel_02/opdracht_1/Sollicitatie.py
@@ -1,3 +1,6 @@
+from random import random
+
+
 print ('+++++++++++++++++++++++++++++'
 'solicitatie Circusdirecteur voor Circus HotelDeBotel'
 """+++++++++++++++++++++++++++++""")
@@ -10,18 +13,26 @@ if geslachtMv == 'm'or geslachtMv == 'M':
 
 elif geslachtMv == 'v' or geslachtMv == 'V':
     vrouwHaarkleur = input("Wat is uw haarkleur? (rood/zwart/blond): ")
+    if vrouwHaarkleur == 'rood':
+        raise NameError('we nemen geen vuurtorens aan')
     vrouwHaarlengte = int(input("Wat is uw krulhaarlengte? (in cm): "))
 dierDressuur = int(input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?: "))
 jongleerErvaring = int(input("Hoeveel jaar ervaring heeft u met jongleren?: "))
 acroErvaring = int(input("Hoeveel jaar praktijkervaring heeft u met acrobatiek?: "))
 randomVraag1 = input("Bent u Minecraft-Expert?: (J/N) ")
 diploma = input("Bent u in bezit van een Diploma MBO-4 ondernemen? (J/N): ")
-rijbewijs = input("Bent u in bezit van een vrachtwagen rijbewijs? (J/N): ")
+if diploma == 'nee':
+    raise NameError("je bent te dom")
+else:
+    rijbewijs = input("Bent u in bezit van een vrachtwagen rijbewijs? (J/N): ")
 randomVraag2 = input("Heeft u ooit een vulkaan-uitbarsting gezien? (J/N): ")
 hogeHoed = input("Bent u in het bezit van een grote hoed? (J/N): ")
 randomVraag3 = input("Spendeert u een hele dag op YouTube? (J/N): ")
 persoonLengte = int(input("Hoe lang bent u? (in cm): "))
-randomVraag4 = input("Wilt u de loterij winnen? (J/N): ")
+if persoonLengte == '150':
+    raise NameError('helaas we nemen geen goblins aan')
+else:
+    randomVraag4 = input("Wilt u de loterij winnen? (J/N): ")
 persoonGewicht = int(input("Hoeveel weegt u? (in kg): "))
 certificaatGevaar = input("Heeft u het certificaat 'Overleven met gevaarlijke mensen'? (J/N) ")
 
