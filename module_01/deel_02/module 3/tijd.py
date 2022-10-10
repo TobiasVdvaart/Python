08f8874ac07f61034ab50e85372a51471f42972b
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-10-10 14:23:02 +0200

    'opdracht3'

---
+++ module_01/deel_02/module 3/tijd.py
@@ -0,0 +1,13 @@
+#klok = 24 
+# while klok < 0:
+#    klok -=1
+#    if klok >13:
+#        print (f'{klok}PM')
+#
+#    if klok <13:
+#        print (f'{klok}AM')
+
+for x in range(12):
+    print(f'{x+1}AM')
+for y in range(12):
+    print(f'{y +1}PM') 
