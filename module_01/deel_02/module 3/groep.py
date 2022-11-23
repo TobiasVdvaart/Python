db34fbc95f9d695d22f8d5e3bb220173bae4ba85
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2022-11-23 10:59:37 +0100

    push

---
+++ module_01/deel_02/module 3/groep.py
@@ -0,0 +1,36 @@
+
+antwoord = []
+
+for x in range (1, 11):
+    antwoord.append(x * 15)
+
+print (antwoord[12])
+
+
+
+
+#1
+mijn_lijst = [5, 12, 19, 27, 3]
+
+#2
+mijn_lijst.append(25)
+print(mijn_lijst)
+
+#3
+print(len(mijn_lijst))
+
+#4
+mijn_lijst.remove(12)
+print(mijn_lijst)
+
+namen_lijst = []
+naam = input("voer een naam in of quit, ")
+while naam != "quit":
+    namen_lijst.append(naam)
+
+#5
+mijn_lijst.pop(0)
+print(mijn_lijst)
+
+#6
+mijn_lijst.insert
