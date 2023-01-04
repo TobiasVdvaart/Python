68783714ef581b981531435cbb490f92ec31590c
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-01-04 16:30:29 +0100

    commit

---
+++ module_04/deel_02/opdracht_01/fruitmand7.py
@@ -0,0 +1,10 @@
+from fruitmand1 import fruitmand
+
+# print(fruitmand)
+
+# if round in fruitmand == True:
+#     print (fruitmand['name'])
+
+for fruit in fruitmand:
+    if (fruit['round']) == True:
+        print(fruit['name'])
