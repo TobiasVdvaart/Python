6abc0cc8d30f2cb5f64d77842d767a0b34a743e0
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-01-09 09:53:03 +0100

    commit

--- module_04/deel_01/opdracht_01/M&M.py
+++ module_04/deel_01/opdracht_01/M&M.py
@@ -4,7 +4,7 @@ kleuren = ('oranje', 'blauw', 'groen', 'bruin')
 zak = []
 aantal = int(input('Hoeveel M&Ms wil je toevoegen aan de zak? '))
 for i in range(aantal):
-    kleur = kleuren[random.randint(0, len(kleuren) - 1)]
+    kleur = kleuren[random.randint(0, (kleuren) - 1)]
     zak.append(kleur)
 
 print('De zak bevat de volgende M&Ms:')
---
+++ module_04/deel_02/opdracht_01/fruitmand10.py
@@ -0,0 +1,9 @@
+from fruitmand1 import fruitmand
+
+gewichten = []
+
+for x in fruitmand:
+    gewichten.append(x["weight"])
+
+print(sorted(gewichten))
+
