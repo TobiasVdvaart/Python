b6af06facc852536a4bfcdb86c53d4135a20dd59
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-01-09 10:04:51 +0100

    commit

--- module_04/deel_02/opdracht_01/fruitmand10.py
+++ module_04/deel_02/opdracht_01/fruitmand10.py
@@ -1,9 +1,15 @@
 from fruitmand1 import fruitmand
 
 gewichten = []
+gesoteerde_fruit = []
 
-for x in fruitmand:
-    gewichten.append(x["weight"])
+for gewicht in fruitmand:
+    gewichten.append(gewicht["weight"])
 
 print(sorted(gewichten))
+
+for fruit in fruitmand:
+    gesoteerde_fruit.append(fruit["name"])
+print(sorted(gesoteerde_fruit))    
+
 
