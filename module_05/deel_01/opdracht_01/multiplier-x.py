9ecce2ee348705169018c77f401f62036a24145f
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-02-16 13:34:40 +0100

    commit

---
+++ module_05/deel_01/opdracht_01/multiplier-x.py
@@ -0,0 +1,14 @@
+def my_function():
+    aantal = int(input("hoeveel keer wilt u het printen?, "))
+    print (aantal * 1)
+    print (aantal * 2)
+    print (aantal * 3)
+    print (aantal * 4)
+    print (aantal * 5)
+    print (aantal * 6)
+    print (aantal * 7)
+    print (aantal * 8)
+    print (aantal * 9)
+    print (aantal * 10)
+
+my_function()
