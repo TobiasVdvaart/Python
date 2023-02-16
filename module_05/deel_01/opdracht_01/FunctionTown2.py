3b4feb956a589c22bd6c6b4a293339e8cc6c0362
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-02-16 13:17:53 +0100

    commit

--- module_05/deel_01/opdracht_01/FunctionTown2.py
+++ module_05/deel_01/opdracht_01/FunctionTown2.py
@@ -3,6 +3,6 @@
 def my_function():
   aantal = int(input("hoeveel keer wilt u het printen?, "))
   for x in range(aantal):
-    print("welcome to function town!")
+    print ("welcome to function town!(aantal)")
 
 my_function()
