aa35bdf677c276b7a92a22fc3c3ec36cd468644d
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-10 09:58:36 +0100

    psyh

---
+++ module_05/deel_01/opdracht_01/Game-info.py
@@ -0,0 +1,7 @@
+#funtie_01 Damage
+#deze funtie berekent de damage als de gebruiker damage aangeeft met de wapen die de gebruiker heeft
+
+#functie_02 Healing
+#
+
+#variables
--- module_05/deel_01/opdracht_01/game_verhaal.py
+++ module_05/deel_01/opdracht_01/game_verhaal.py
@@ -1,4 +1,20 @@
 Teksten = (
-    " WELKOM BIJ DE HUNTERS PARADISE ",
-    "string 2"
+    " Welcome to Hunter's Paradise would you like to start the story?  ",
+    "This is the story about a young man with a dream to become the best hunter"
+)
+
+afbeeldingen = (
+    """          _______
+         /      /
+       /      //
+      /______//
+     (______(/ """,
+
+
+  """         ______ ______
+       _/      Y      \_
+      // ~~ ~~ | ~~ ~  \\
+     // ~ ~ ~~ | ~~~ ~~ \\       
+    //________.|.________\\     
+`----------`-'----------'""",
 )
