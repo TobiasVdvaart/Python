4f115a1bf1fff42740522459a40707e02f58b031
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-10 09:58:25 +0100

    commit

---
+++ module_05/deel_01/opdracht_01/HunterParadise.py
@@ -0,0 +1,15 @@
+from game_verhaal import *
+
+print(afbeeldingen[0])
+print(Teksten[0])
+vraag_1 = input(" yes/no, ")
+
+if vraag_1 != "yes":
+    quit
+
+naam = "wat is uw naam?, "
+
+print(afbeeldingen[1])
+print(Teksten[1])
+
+
