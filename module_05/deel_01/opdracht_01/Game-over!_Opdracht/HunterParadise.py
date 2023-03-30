46b2afe6666a3e7cb84bb3aca3e9d67803f010bf
tobias van der vaart <100384260+TobiasVaart@users.noreply.github.com>
2023-03-30 09:37:44 +0200

    push

--- module_05/deel_01/opdracht_01/Game-over!_Opdracht/HunterParadise.py
+++ module_05/deel_01/opdracht_01/Game-over!_Opdracht/HunterParadise.py
@@ -2,11 +2,11 @@ from game_verhaal import *
 import random
 import time
 
-player_hp = 100
 player_attack = 25
+
 player_defense = 0
 
-goblin_hp = 80
+
 goblin_attack = 20
 
 start_vraag = input("wilt u starten? yes/no ")
@@ -19,23 +19,14 @@ else:
     time.sleep(2)
     print(Teksten[0])
     time.sleep(2)
-    while goblin_hp > 0:
-        print(f"Goblin: HP:{goblin_hp} Attack:{goblin_attack}")
-        speler_keuze_1 = input(f"{naam_speler}: HP:{player_hp} Attack:{player_attack} Defense:{player_defense}. Wat gaat u doen: attack/defense ")
-        
-        if speler_keuze_1 == 'attack':
-            goblin_hp = goblin_hp - random.randint(15, 25)
-            time.sleep(1)
-            print(f'de Goblins hp is nu: {goblin_hp}')
-            time.sleep(1)
-            print('de goblin heeft ook aangevallen!')
-            player_hp = player_hp - random.randint(10, 20)
-            time.sleep(0)
-            print(f'je HP is nu:{player_hp}')
 
-        if player_defense == 0:
-            if speler_keuze_1 == 'defense':
-                print('Je heb geen schild!')
+    def gevecht(player_hp,goblin_hp):
+        while goblin_hp > 0:
+            print(f"Goblin: HP:{goblin_hp} Attack:{goblin_attack}")
+            speler_keuze_1 = input(f"{naam_speler}: HP:{player_hp} Attack:{player_attack} Defense:{player_defense}. Wat gaat u doen: attack/defense ")
+
+            if speler_keuze_1 == 'attack':
+                goblin_hp = goblin_hp - random.randint(15, 25)
                 time.sleep(1)
                 print(f'de Goblins hp is nu: {goblin_hp}')
                 time.sleep(1)
@@ -44,27 +35,115 @@ else:
                 time.sleep(0)
                 print(f'je HP is nu:{player_hp}')
 
-        if goblin_hp <= 0:
-            player_hp = 100
-            player_hp = player_hp + 30
-            player_attack = player_attack + random.randint(10,20)
-            print("Je bent level Up!")
-            time.sleep(2)
-            print(f'je HP is nu {player_hp}')
-            time.sleep(2)
-            print("FLOOR REWARD")
-            time.sleep(2)
-            print('Fire sword')
-            time.sleep(2)
-            print(f'Je attack is nu {player_attack}')
-            time.sleep(2)
-            print("je heb de 1ste monster verslagen...")
-            time.sleep(3)
-            print("maar wees niet te blij want dit is niet de laaste!")
-            time.sleep(1)
-            goblin_hp = 80
-            goblin_hp = goblin_hp + random.randint(30, 50)
-            goblin_attack = goblin_attack + random.randint(5, 10)	
-            while goblin_hp > 0:
-                print(f"Goblin: HP:{goblin_hp} Attack:{goblin_attack}")
-                speler_keuze_1 = input(f"{naam_speler}: HP:{player_hp} Attack:{player_attack} Defense:{player_defense}. Wat gaat u doen: attack/defense ")
+            if player_defense == 0:
+                if speler_keuze_1 == 'defense':
+                    print('Je hebt geen schild!')
+                    time.sleep(1)
+                    print(f'de Goblins hp is nu: {goblin_hp}')
+                    time.sleep(1)
+                    print('de goblin heeft ook aangevallen!')
+                    player_hp = player_hp - random.randint(10, 20)
+                    time.sleep(0)
+                    print(f'je HP is nu:{player_hp}')
+        return goblin_hp, player_hp
+    gevecht(100,80)
+
+
+    player_hp = 100
+    player_hp = player_hp + 30
+    player_attack = player_attack + random.randint(10,20)
+    print("Je bent level Up!")
+    time.sleep(2)
+    print(f'je HP is nu {player_hp}')
+    time.sleep(2)
+    print("FLOOR REWARD")
+    time.sleep(2)
+    print('Fire sword')
+    time.sleep(2)
+    print(f'Je attack is nu {player_attack}')
+    time.sleep(2)
+    print("je hebt de 1ste monster verslagen...")
+    time.sleep(3)
+    print("maar wees niet te blij want dit is niet de laatste!")
+    time.sleep(1)
+    goblin_hp = 80
+    goblin_hp = goblin_hp + random.randint(30, 50)
+    goblin_attack = goblin_attack + random.randint(5, 10)
+
+    gevecht()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # while goblin_hp > 0:
+    #     print(f"Goblin: HP:{goblin_hp} Attack:{goblin_attack}")
+    #     speler_keuze_1 = input(f"{naam_speler}: HP:{player_hp} Attack:{player_attack} Defense:{player_defense}. Wat gaat u doen: attack/defense ")
+        
+    #     if speler_keuze_1 == 'attack':
+    #         goblin_hp = goblin_hp - random.randint(15, 25)
+    #         time.sleep(1)
+    #         print(f'de Goblins hp is nu: {goblin_hp}')
+    #         time.sleep(1)
+    #         print('de goblin heeft ook aangevallen!')
+    #         player_hp = player_hp - random.randint(10, 20)
+    #         time.sleep(0)
+    #         print(f'je HP is nu:{player_hp}')
+
+    #     if player_defense == 0:
+    #         if speler_keuze_1 == 'defense':
+    #             print('Je heb geen schild!')
+    #             time.sleep(1)
+    #             print(f'de Goblins hp is nu: {goblin_hp}')
+    #             time.sleep(1)
+    #             print('de goblin heeft ook aangevallen!')
+    #             player_hp = player_hp - random.randint(10, 20)
+    #             time.sleep(0)
+    #             print(f'je HP is nu:{player_hp}')
+
+    #     if goblin_hp <= 0:
+    #         player_hp = 100
+    #         player_hp = player_hp + 30
+    #         player_attack = player_attack + random.randint(10,20)
+    #         print("Je bent level Up!")
+    #         time.sleep(2)
+    #         print(f'je HP is nu {player_hp}')
+    #         time.sleep(2)
+    #         print("FLOOR REWARD")
+    #         time.sleep(2)
+    #         print('Fire sword')
+    #         time.sleep(2)
+    #         print(f'Je attack is nu {player_attack}')
+    #         time.sleep(2)
+    #         print("je heb de 1ste monster verslagen...")
+    #         time.sleep(3)
+    #         print("maar wees niet te blij want dit is niet de laaste!")
+    #         time.sleep(1)
+    #         goblin_hp = 80
+    #         goblin_hp = goblin_hp + random.randint(30, 50)
+    #         goblin_attack = goblin_attack + random.randint(5, 10)	
+    #         while goblin_hp > 0:
+    #             print(f"Goblin: HP:{goblin_hp} Attack:{goblin_attack}")
+    #             speler_keuze_1 = input(f"{naam_speler}: HP:{player_hp} Attack:{player_attack} Defense:{player_defense}. Wat gaat u doen: attack/defense ")
